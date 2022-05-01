00f64fe6f8ec36fdc7b848eba5fc77c8e896cf20
Zhemchuzhnyi Andrii <n3kk3ll@gmail.com>
2022-05-01 17:22:59 +0300

    got working ranges

--- 03-CSS-variables/js/app.js
+++ 03-CSS-variables/js/app.js
@@ -2,9 +2,17 @@
 
 window.addEventListener(`DOMContentLoaded`, () => {
 
-  function changeVariables () {
+  function changeVariables() {
 
-    
+    const inputs = document.querySelectorAll(`input`);
+
+
+    function changeValue() {
+      const suffix = this.dataset.sizing || ``;
+      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    }
+
+    inputs.forEach(input => input.addEventListener(`input`, changeValue));
 
   }
 
