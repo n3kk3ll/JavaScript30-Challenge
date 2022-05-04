8c4666d6a4c650c7f6fc35134d592898af17d342
Zhemchuzhnyi Andrii <n3kk3ll@gmail.com>
2022-05-04 13:51:22 +0300

    day 06 layout

---
+++ 06-Type-ahead/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.addEventListener(`DOMContentLoaded`, () => {
+
+  function searchCity () {
+    const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+  }
+
+  searchCity();
+});
