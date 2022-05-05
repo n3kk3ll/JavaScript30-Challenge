1afa0596391df7652b29e4ffd959c33321a5e6ed
Zhemchuzhnyi Andrii <n3kk3ll@gmail.com>
2022-05-05 15:52:02 +0300

    day 07 over

--- 07-Array-cardio-day-2/js/app.js
+++ 07-Array-cardio-day-2/js/app.js
@@ -19,16 +19,37 @@ window.addEventListener(`DOMContentLoaded`, () => {
     { text: 'Nice Nice Nice!', id: 542328 }
   ];
 
+  const year = new Date().getFullYear();
+
   // Some and Every Checks
   // Array.prototype.some() // is at least one person 19 or older?
+  const isSomebodyOlder = arr => arr.some(item => (year - item.year) >= 19);
+  console.log(isSomebodyOlder(people));
+
   // Array.prototype.every() // is everyone 19 or older?
+  const isEverybodyOlder = arr => arr.every(item => (year - item.year) >= 19);
+  console.log(isEverybodyOlder(people));
 
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
+  const findComment = (arr, value) => {
+    return arr.find(item => item.id === value);
+  };
+  console.log(findComment(comments, 823423));
 
   // Array.prototype.findIndex()
   // Find the comment with this ID
   // delete the comment with the ID of 823423
-
+  // comments.splice(1,1);
+  // console.log(comments);
+  const findCommentIndex = (arr, value) => {
+    const index = arr.findIndex(item => item.id === value);
+    return index;
+  };
+  const deleteComment = (arr, index) => {
+    arr.splice(index, 1);
+    return arr;
+  };
+  console.log(deleteComment(comments, findCommentIndex(comments, 823423)));
 });
